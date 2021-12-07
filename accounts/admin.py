4cbd8d541450b67b2252a9e5dec82fc04ecf561a
fpringle <freddyjepringle@gmail.com>
2021-12-07 18:48:04 +0100

    moved admin classes to the right apps

--- accounts/admin.py
+++ accounts/admin.py
@@ -11,80 +11,10 @@ from .models import (
     SpotifyUser,
     SpotifyUserCredentials,
 )
-from music.models import (
-    UserPlaylist,
-    Track,
-    TrackFeatures,
-)
+
+from music.admin import UserPlaylistInlineAdmin
 
 logger = logging.getLogger("admin")
-
-
-class TrackFeaturesInlineAdmin(admin.StackedInline):
-    model = TrackFeatures
-
-    def has_change_permission(self, req, obj=None):
-        return False
-
-class TrackFeaturesAdmin(admin.ModelAdmin):
-    model = TrackFeatures
-
-class TrackAdmin(admin.ModelAdmin):
-    model = Track
-    fieldsets = [
-        ("Track info", {"fields": ["spotify_id", "name", "artist_list", "album"]}),
-    ]
-    inlines = [TrackFeaturesInlineAdmin]
-
-    @admin.display(description="Artists")
-    def artist_list(self, obj):
-        return ", ".join(obj.artist_list)
-
-    def has_change_permission(self, req, obj=None):
-        return False
-
-
-class TrackInlineAdmin(admin.TabularInline):
-    model = Track
-
-
-class UserPlaylistInlineAdmin(admin.TabularInline):
-    model = UserPlaylist
-    extra = 0
-    fields = ("name", "link", "spotify_id", "snapshot_id", "last_updated")
-    readonly_fields = ("link", "last_updated")
-
-    @admin.display(description="Link")
-    def link(self, obj):
-        return mark_safe(
-            '<a href="{}">{}</a>'.format(
-                reverse("admin:music_userplaylist_change", args=(obj.pk,)), obj.name
-            )
-        )
-
-
-class UserPlaylistAdmin(admin.ModelAdmin):
-    model = UserPlaylist
-    fields = (
-        "spotify_id",
-        "snapshot_id",
-        "name",
-        "user",
-        "tracks",
-        "last_updated",
-    )
-    readonly_fields = ("tracks", "last_updated")
-
-    def tracks(self, obj):
-        def get_link(track):
-            return '<a href="{}">{}</a>'.format(
-                reverse("admin:music_track_change", args=(track.pk,)), track.name
-            )
-
-        def get_li(track):
-            return "<li>" + get_link(track) + "</li>"
-
-        return mark_safe("<ul>" + "".join(map(get_li, obj.track_set.all())) + "</ul>")
 
 
 class SpotifyUserInlineAdmin(admin.StackedInline):
@@ -135,8 +65,5 @@ class RegistrationStateAdmin(admin.ModelAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
-admin.site.register(Track, TrackAdmin)
 admin.site.register(SpotifyUser, SpotifyUserAdmin)
-admin.site.register(UserPlaylist, UserPlaylistAdmin)
 admin.site.register(RegistrationState, RegistrationStateAdmin)
-admin.site.register(TrackFeatures, TrackFeaturesAdmin)
--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,77 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.urls import reverse
 
-# Register your models here.
+from .models import Track, TrackFeatures, UserPlaylist
+
+
+class TrackFeaturesInlineAdmin(admin.StackedInline):
+    model = TrackFeatures
+
+    def has_change_permission(self, req, obj=None):
+        return False
+
+class TrackFeaturesAdmin(admin.ModelAdmin):
+    model = TrackFeatures
+
+    def has_change_permission(self, req, obj=None):
+        return False
+
+class TrackAdmin(admin.ModelAdmin):
+    model = Track
+    fieldsets = [
+        ("Track info", {"fields": ["spotify_id", "name", "artist_list", "album"]}),
+    ]
+    inlines = [TrackFeaturesInlineAdmin]
+
+    @admin.display(description="Artists")
+    def artist_list(self, obj):
+        return ", ".join(obj.artist_list)
+
+    def has_change_permission(self, req, obj=None):
+        return False
+
+class TrackInlineAdmin(admin.TabularInline):
+    model = Track
+
+class UserPlaylistAdmin(admin.ModelAdmin):
+    model = UserPlaylist
+    fields = (
+        "spotify_id",
+        "snapshot_id",
+        "name",
+        "user",
+        "tracks",
+        "last_updated",
+    )
+    readonly_fields = ("tracks", "last_updated")
+
+    def tracks(self, obj):
+        def get_link(track):
+            return '<a href="{}">{}</a>'.format(
+                reverse("admin:music_track_change", args=(track.pk,)), track.name
+            )
+
+        def get_li(track):
+            return "<li>" + get_link(track) + "</li>"
+
+        return mark_safe("<ul>" + "".join(map(get_li, obj.track_set.all())) + "</ul>")
+
+class UserPlaylistInlineAdmin(admin.TabularInline):
+    model = UserPlaylist
+    extra = 0
+    fields = ("name", "link", "spotify_id", "snapshot_id", "last_updated")
+    readonly_fields = ("link", "last_updated")
+
+    @admin.display(description="Link")
+    def link(self, obj):
+        return mark_safe(
+            '<a href="{}">{}</a>'.format(
+                reverse("admin:music_userplaylist_change", args=(obj.pk,)), obj.name
+            )
+        )
+
+
+admin.site.register(Track, TrackAdmin)
+admin.site.register(UserPlaylist, UserPlaylistAdmin)
+admin.site.register(TrackFeatures, TrackFeaturesAdmin)
