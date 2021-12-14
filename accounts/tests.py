c74fb5c3796f47a5b2878ea8da3668034b4c9da6
fpringle <freddyjepringle@gmail.com>
2021-12-14 13:06:13 +0100

    registrationstate tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,1 +1,46 @@
-from django.test import TestCase  # noqa
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import RegistrationState, SpotifyUser, SpotifyUserCredentials, User  # noqa
+
+
+class RegistrationStateTestCase(TestCase):
+    def test_state_strings_are_unique(self):
+        """
+        Check if the randomly-generated state strings are guaranteed unique
+        By limiting the length of the strings to 2, the total nummber of unique
+        strings is 62 * 62 = 3844. If we do 250 iterations, by the birthday problem
+        (https://en.wikipedia.org/wiki/Birthday_problem), the chances of 2 strings
+        being equal (with normal random choices) is 99.97%. So if none of these
+        are equal, we can be certain that the uniqueness method is working.
+        """
+        strings = set()
+        for _ in range(250):
+            state_string = RegistrationState.unique_random_string(2)
+            self.assertNotIn(state_string, strings)
+            strings.add(state_string)
+            reg = RegistrationState.objects.create()
+            reg.state_string = state_string
+            reg.save()
+
+    def setUp(self):
+        RegistrationState.objects.all().delete()
+
+    def test_drop_older_than(self):
+        def strings():
+            return sorted(reg.state_string for reg in RegistrationState.objects.all())
+
+        self.assertEqual(strings(), [])
+
+        times = [(2021, 12, 14, 11), (2021, 12, 14, 12), (2021, 12, 14, 13)]
+        times = [timezone.datetime(*args, tzinfo=timezone.utc) for args in times]
+        for time in times:
+            reg = RegistrationState()
+            reg.save()
+            reg.state_string = str(time.hour)
+            reg.created_at = time
+            reg.save()
+
+        self.assertEqual(strings(), ["11", "12", "13"])
+        RegistrationState.drop_before(times[2])
+        self.assertEqual(strings(), ["13"])
